3b920896e346bb9565e263affb711ebf821d62c4
pasindup <pasindup.17@itfac.mrt.ac.lk>
2025-11-09 02:34:27 +0530

    initial basic setup

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from app.db import db
+from app.utils.loader import load_routes_from_yaml
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("app.config.Config")
+
+    # Initialize database
+    db.init_app(app)
+
+    # Load routes dynamically from routes.yaml
+    load_routes_from_yaml(app, "app/routes.yaml")
+
+    with app.app_context():
+        db.create_all()  # Create tables on first run
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///mvp.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
