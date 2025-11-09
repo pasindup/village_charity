135c73955cf812d67e0b729cc660992bf1d2d1d6
pasindup <pasindup.17@itfac.mrt.ac.lk>
2025-11-09 13:00:59 +0530

    quick add

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from app.db import db
 from app.utils.loader import load_routes_from_yaml
+from flask_migrate import Migrate
 
+
+migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
 
     # Initialize database
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Load routes dynamically from routes.yaml
     load_routes_from_yaml(app, "app/routes.yaml")
